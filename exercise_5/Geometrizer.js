d39fac8b0555c4e01ac7962841e0e5b2a1300989
Viola <justinviola@gmail.com>
2023-04-13 10:47:06 -0400

    Geometrizer.js

---
+++ exercise_5/Geometrizer.js
@@ -0,0 +1,19 @@
+
+ function calcCircumfrence(circleRadius)
+ {
+    //  var circleRadius = parseInt(prompt("Please enter the radius of circle:"));
+     var circumferenceOfCircle = 2 * Math.PI * circleRadius;
+     console.log("Circumference of circle is: " + circumferenceOfCircle);
+ }
+ calcCircumfrence(5)
+ calcCircumfrence(8)
+
+ function calcArea(circleRadius)
+ {
+    var areaofCircle= 3.14 * circleRadius * circleRadius;
+    console.log("The area of a circle is:" + areaofCircle );
+ }
+ calcArea(5)
+ calcArea(9)
+
+
