345b7175ab306c9a6bf7fa1dd1cc3ae4fe6533ac
Viola <justinviola@gmail.com>
2023-04-18 17:17:38 -0400

    Input String is a palindrome

---
+++ exercise_7/CheckPalindrome.js
@@ -0,0 +1,14 @@
+function palindrome(str) {
+    var re = /[^A-Za-z0-9]/g;
+    str = str.toLowerCase().replace(re, '');
+    var len = str.length;
+    for (var i = 0; i < len/2; i++) {
+      if (str[i] !== str[len - 1 - i]) {
+          return false;
+      }
+    }
+    return true;
+   }
+   console.log(`Is given string Palindrome ? `+ palindrome("Door"));
+
+
